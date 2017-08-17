3224309affaa231e4474ec592216531f00d00741
akshayscu <akshay.gadegone@gmail.com>
2017-08-16 21:25:39 -0700

    changed the API service

--- main.js
+++ main.js
@@ -7,75 +7,81 @@ $(document).ready(function(){
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      var api = "https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=7lX0OuSF5hvFQJVSFMjUz49bJt5V6Fdb&q="+lat+"%2C"+long;
+      var locationAPI = "https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=7lX0OuSF5hvFQJVSFMjUz49bJt5V6Fdb&q="+lat+"%2C"+long;
       console.log(api);
 
+      // $.getJSON(api,function(data){
+      //
+      //   weatherType = data.weather[0].description;
+      //   kelvin = data.main.temp;
+      //   fahrenheit = (kelvin)*(9/5)-459.67;
+      //   celsius = kelvin-273;
+      //   city = data.name;
+      //   windSpeed = data.wind.speed;
+      //   mph = windSpeed*2.2369;
+      //
+      //   $('#city').html(city);
+      //   $('#weatherType').html(weatherType);
+      //   //$('#temp').html(fahrenheit.toFixed(1)+' &#8457;');
+      //   $('#temp').html(fahrenheit.toFixed(1));
+      //   $('#windSpeed').html(mph.toFixed(1)+" mph");
+      //
+      //   $('#tog').on('change',function(){
+      //     if(!$(this).prop('checked')){
+      //       $('#temp').html(celsius.toFixed(1));
+      //     }
+      //     else{
+      //       $('#temp').html(fahrenheit.toFixed(1));
+      //     }
+      //   });
+      //
+      //   var conditions = weatherType.split(" ");
+      //   var flag = false;
+      //   for(var i=0;i<conditions.length;i++){
+      //     if(/clear/.test(conditions[i])===true){
+      //       $('body').css("background-image","url('img/clear.jpg')");
+      //       console.log("in clear");
+      //       flag=true;
+      //     }
+      //     else if(/cloud/.test(conditions[i])===true){
+      //       console.log("in cloud");
+      //       $('body').css("background-image","url('img/cloudy.jpg')");
+      //       flag=true;
+      //     }
+      //     else if(/rain/.test(conditions[i])===true){
+      //       console.log("in rain");
+      //       $('body').css("background-image","url('img/rainy.jpg')");
+      //       flag=true;
+      //     }
+      //     else if(/snow/.test(conditions[i])===true){
+      //       console.log("in snow");
+      //       $('body').css("background-image","url('img/snow.jpg')");
+      //       flag=true;
+      //     }
+      //     else if(/fog/.test(conditions[i])===true){
+      //       console.log("in fog");
+      //       $('body').css("background-image","url('img/foggy.jpg')");
+      //       flag=true;
+      //     }
+      //     else if(/haz(e|y)|mist/.test(conditions[i])===true){
+      //       console.log("in haze");
+      //       $('body').css("background-image","url('img/hazy.jpg')");
+      //       flag=true;
+      //     }
+      //   }
+      //
+      //   if(flag===false){
+      //     console.log("in none");
+      //     $('body').css("background-image","url('img/placeholder.jpg')");
+      //   }
+      //
+      //   });
+      var key = 0;
       $.getJSON(api,function(data){
-
-        weatherType = data.weather[0].description;
-        kelvin = data.main.temp;
-        fahrenheit = (kelvin)*(9/5)-459.67;
-        celsius = kelvin-273;
-        city = data.name;
-        windSpeed = data.wind.speed;
-        mph = windSpeed*2.2369;
-
-        $('#city').html(city);
-        $('#weatherType').html(weatherType);
-        //$('#temp').html(fahrenheit.toFixed(1)+' &#8457;');
-        $('#temp').html(fahrenheit.toFixed(1));
-        $('#windSpeed').html(mph.toFixed(1)+" mph");
-
-        $('#tog').on('change',function(){
-          if(!$(this).prop('checked')){
-            $('#temp').html(celsius.toFixed(1));
-          }
-          else{
-            $('#temp').html(fahrenheit.toFixed(1));
-          }
-        });
-
-        var conditions = weatherType.split(" ");
-        var flag = false;
-        for(var i=0;i<conditions.length;i++){
-          if(/clear/.test(conditions[i])===true){
-            $('body').css("background-image","url('img/clear.jpg')");
-            console.log("in clear");
-            flag=true;
-          }
-          else if(/cloud/.test(conditions[i])===true){
-            console.log("in cloud");
-            $('body').css("background-image","url('img/cloudy.jpg')");
-            flag=true;
-          }
-          else if(/rain/.test(conditions[i])===true){
-            console.log("in rain");
-            $('body').css("background-image","url('img/rainy.jpg')");
-            flag=true;
-          }
-          else if(/snow/.test(conditions[i])===true){
-            console.log("in snow");
-            $('body').css("background-image","url('img/snow.jpg')");
-            flag=true;
-          }
-          else if(/fog/.test(conditions[i])===true){
-            console.log("in fog");
-            $('body').css("background-image","url('img/foggy.jpg')");
-            flag=true;
-          }
-          else if(/haz(e|y)|mist/.test(conditions[i])===true){
-            console.log("in haze");
-            $('body').css("background-image","url('img/hazy.jpg')");
-            flag=true;
-          }
-        }
-
-        if(flag===false){
-          console.log("in none");
-          $('body').css("background-image","url('img/placeholder.jpg')");
-        }
-
-        });
+          key = data.Key;
       });
-    }
-  });
+      var forecastAPI = "https://dataservice.accuweather.com/forecasts/v1/hourly/1hour/"+key+"?apikey=7lX0OuSF5hvFQJVSFMjUz49bJt5V6Fdb";
+      console.log(forecastAPI);
+    });
+  }
+});
