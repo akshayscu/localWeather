d5d2813d6934ee7a29e4d8bb13047a2b664426eb
Akshay Gadegone <akshay.gadegone@gmail.com>
2017-08-19 12:09:50 -0700

    code cleanup

--- main.js
+++ main.js
@@ -2,10 +2,6 @@ $(document).ready(function(){
 
   var long,lat,weatherType,celsius,fahrenheit,area,key,locationAPI,forecastAPI,link;
 
-  // mph = windSpeed*2.2369;
-  // celsius = kelvin-273;
-  // fahrenheit = (kelvin)*(9/5)-459.67;
-
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){
       long = position.coords.longitude;
@@ -81,75 +77,6 @@ $(document).ready(function(){
               }
           });
       });
-
-
     });
   }
 });
-
-// $.getJSON(api,function(data){
-      //
-      //   weatherType = data.weather[0].description;
-      //   kelvin = data.main.temp;
-      //   fahrenheit = (kelvin)*(9/5)-459.67;
-      //   celsius = kelvin-273;
-      //   city = data.name;
-      //   windSpeed = data.wind.speed;
-      //   mph = windSpeed*2.2369;
-      //
-      //   $('#city').html(city);
-      //   $('#weatherType').html(weatherType);
-      //   //$('#temp').html(fahrenheit.toFixed(1)+' &#8457;');
-      //   $('#temp').html(fahrenheit.toFixed(1));
-      //   $('#windSpeed').html(mph.toFixed(1)+" mph");
-      //
-      //   $('#tog').on('change',function(){
-      //     if(!$(this).prop('checked')){
-      //       $('#temp').html(celsius.toFixed(1));
-      //     }
-      //     else{
-      //       $('#temp').html(fahrenheit.toFixed(1));
-      //     }
-      //   });
-      //
-      //   var conditions = weatherType.split(" ");
-      //   var flag = false;
-      //   for(var i=0;i<conditions.length;i++){
-      //     if(/clear/.test(conditions[i])===true){
-      //       $('body').css("background-image","url('img/clear.jpg')");
-      //       console.log("in clear");
-      //       flag=true;
-      //     }
-      //     else if(/cloud/.test(conditions[i])===true){
-      //       console.log("in cloud");
-      //       $('body').css("background-image","url('img/cloudy.jpg')");
-      //       flag=true;
-      //     }
-      //     else if(/rain/.test(conditions[i])===true){
-      //       console.log("in rain");
-      //       $('body').css("background-image","url('img/rainy.jpg')");
-      //       flag=true;
-      //     }
-      //     else if(/snow/.test(conditions[i])===true){
-      //       console.log("in snow");
-      //       $('body').css("background-image","url('img/snow.jpg')");
-      //       flag=true;
-      //     }
-      //     else if(/fog/.test(conditions[i])===true){
-      //       console.log("in fog");
-      //       $('body').css("background-image","url('img/foggy.jpg')");
-      //       flag=true;
-      //     }
-      //     else if(/haz(e|y)|mist/.test(conditions[i])===true){
-      //       console.log("in haze");
-      //       $('body').css("background-image","url('img/hazy.jpg')");
-      //       flag=true;
-      //     }
-      //   }
-      //
-      //   if(flag===false){
-      //     console.log("in none");
-      //     $('body').css("background-image","url('img/placeholder.jpg')");
-      //   }
-      //
-      //   });
