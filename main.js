9fe7b3c9643dfc43eccc6bb1a0c647f492153790
Akshay Gadegone <akshay.gadegone@gmail.com>
2017-08-26 13:17:50 -0700

    fixed reg. exp...

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ $(document).ready(function() {
               var conditions = weatherType.split(" ");
               var flag = false;
               for (var i = 0; i < conditions.length; i++) {
-                if (/clear|sun/.test(conditions[i]) === true && dayTime === true) {
+                if (/clear|(S|s)un/.test(conditions[i]) === true && dayTime === true) {
                   $('body').css("background-image", "url('img/clear.jpg')");
                   console.log("in clear");
                   flag = true;
